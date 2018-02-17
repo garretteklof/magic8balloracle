09a65e1a56812b669a32dbbe6ef5555fdde37144
garretteklof <garrett.eklof@gmail.com>
2018-02-17 16:28:06 -0600

    actually setup webpack correctly for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = (env) => {
 	const isProduction = env === 'production';
 	const CSSExtract = new ExtractTextPlugin('styles.css');
 	return {
-		entry: './src/app.js',
+		entry: ['babel-polyfill', './src/app.js'],
 		output: {
-			path: path.join(__dirname, 'public'),
+			path: path.join(__dirname, 'public', 'dist'),
 			filename: 'bundle.js'
 		},
 		module: {
@@ -40,7 +40,9 @@ module.exports = (env) => {
 		],
 		devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
-			contentBase: path.join(__dirname, 'public')
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+			publicPath: '/dist/'
 		}
 	}
 };
